814801d051f7988eb2200685bfc049417e02307e
Ishi Tran <berugen1st@gmail.com>
2023-01-21 18:38:57 +0700

    feat: add virtual scroll for iteration list

---
+++ src/components/FinishedIteration.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { h, onMounted, onUnmounted, ref } from 'vue';
+import { invoke } from '@tauri-apps/api';
+import { SnakeIteration } from '../types';
+import { RouterLink } from 'vue-router';
+import { useFormattedDate } from '../constants';
+import { RecycleScroller } from 'vue-virtual-scroller';
+import { useIterationRoute } from '../store';
+import { NList, NListItem, NThing, NText, NSpace, NStatistic } from 'naive-ui';
+
+const { setIterationId } = useIterationRoute();
+const iterations = ref<Array<SnakeIteration>>([]);
+
+onMounted(() => {
+  invoke('get_joining_iterations')
+    .then((res) => (iterations.value = res as []))
+    .catch((e) => console.log(e));
+});
+onUnmounted(() => (iterations.value = []));
+</script>
+
+<template>
+  <RecycleScroller
+    class="scroller"
+    :items="iterations"
+    v-slot="{ item }"
+    :item-size="129"
+    :list-tag="
+      h(NList, {
+        hoverable: true,
+        clickable: true,
+        bordered: true,
+      })
+    "
+    :item-tag="NListItem"
+  >
+    <RouterLink
+      :to="`/iterations/${item.id}`"
+      style="text-decoration: none"
+      @click="setIterationId(item.id)"
+    >
+      <NThing content-style="font-size: 18px">
+        <template #header>
+          <NText strong>{{ item.title }}</NText>
+        </template>
+        <NSpace justify="space-around">
+          <NStatistic
+            label="Id"
+            :value="`#${item.id}`"
+          />
+          <NStatistic
+            label="Current Point"
+            :value="item.current_point"
+          />
+          <NStatistic
+            label="Total Point"
+            :value="item.total_point"
+          />
+          <NStatistic
+            label="Created By"
+            :value="item.created_by"
+          />
+          <NStatistic
+            label="End Date"
+            :value="useFormattedDate(item?.end_date)"
+          />
+        </NSpace>
+      </NThing>
+    </RouterLink>
+  </RecycleScroller>
+</template>
